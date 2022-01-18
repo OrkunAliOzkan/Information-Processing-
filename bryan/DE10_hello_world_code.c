edb808a1b255bc955cdf17afa3aca9e329f907dd
bet20ICL <bet20@ic.ac.uk>
2022-01-18 17:25:41 +0000

    added missing hello world c program

---
+++ bryan/DE10_hello_world_code.c
@@ -0,0 +1,17 @@
+#include <sys/alt_stdio.h>
+#include <stdio.h>
+#include "altera_avalon_pio_regs.h"
+#include "system.h"
+
+int main()
+{
+	int switch_datain;
+	alt_putstr("Hello from Nios II!\n");
+
+	while (1){
+		switch_datain = ~IORD_ALTERA_AVALON_PIO_DATA(BUTTON_BASE);
+		switch_datain &= (0b0000001111);
+		IOWR_ALTERA_AVALON_PIO_ADATA(LED_BASE,switch_datain);
+	}
+	return 0;
+}
